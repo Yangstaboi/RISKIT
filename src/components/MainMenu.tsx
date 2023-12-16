ae009c5f75145000cfda61f5904375ba37118409
William <geazymvp@gmail.com>
2023-12-17 12:21:18 +1300

    Changed the colour scheme of the webiste to be majority purple and moved the title of game cards to the top

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -4,11 +4,11 @@ const MainMenu = () => {
       <div className="welcome-button">Welcome to RISKIT</div>
       <div className="game-selection">
         <button className="game-button">Minesweeper</button>
+
         <button className="game-button">Roulette</button>
         <button className="game-button">RISKIT</button>
         <button className="game-button">Poker</button>
         <button className="game-button">BlackJack</button>
-        <button className="game-button">Bacarat</button>
         <button className="game-button">Bacarat</button>
       </div>
     </div>
