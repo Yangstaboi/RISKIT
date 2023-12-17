ee556adf335603f928d336d3eab5bfff77691670
William <geazymvp@gmail.com>
2023-12-17 22:59:47 +1300

    added a placeholder picture and added some outlines for the game cards

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,15 +1,53 @@
+import testingImage from "../assets/testing.png";
+
 const MainMenu = () => {
   return (
     <div className="menu-container">
       <div className="welcome-button">Welcome to RISKIT</div>
       <div className="game-selection">
-        <button className="game-button">Minesweeper</button>
-
-        <button className="game-button">Roulette</button>
-        <button className="game-button">RISKIT</button>
-        <button className="game-button">Poker</button>
-        <button className="game-button">BlackJack</button>
-        <button className="game-button">Bacarat</button>
+        <div className="game-card">
+          <div
+            className="card-image"
+            style={{ backgroundImage: `url(${testingImage})` }}
+          ></div>
+          <div className="card-title">Poker</div>
+          <p>This is a brief description</p>
+        </div>
+        <div className="game-card">
+          <div
+            className="card-image"
+            style={{ backgroundImage: `url(${testingImage})` }}
+          ></div>
+          <h2>Roulette</h2>
+        </div>
+        <div className="game-card">
+          <div
+            className="card-image"
+            style={{ backgroundImage: `url(${testingImage})` }}
+          ></div>
+          <h2>RISKIT</h2>
+        </div>
+        <div className="game-card">
+          <div
+            className="card-image"
+            style={{ backgroundImage: `url(${testingImage})` }}
+          ></div>
+          <h2>Poker</h2>
+        </div>
+        <div className="game-card">
+          <div
+            className="card-image"
+            style={{ backgroundImage: `url(${testingImage})` }}
+          ></div>
+          <h2>BlackJack</h2>
+        </div>
+        <div className="game-card">
+          <div
+            className="card-image"
+            style={{ backgroundImage: `url(${testingImage})` }}
+          ></div>
+          <h2>Bacarat</h2>
+        </div>
       </div>
     </div>
   );
