a28b900cf9ee56b7b9091d10286f204eeffd4615
Mitchell Waghorn <m.waghorn2000@gmail.com>
2024-01-05 18:49:39 +1100

    put the roulette board into a circle

--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -9,7 +9,9 @@ interface RouletteProps {
 }
 
 const numbers = [0, 32, 15, 19, 4, 21, 2, 25,17,34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
-
+const red = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36];
+const black = [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35];
+const purple = [0];
 const Roulette: React.FC<RouletteProps> = ({
   onHomeClick,
   updatePlayerMoney,
@@ -34,10 +36,13 @@ const Roulette: React.FC<RouletteProps> = ({
     <div className="roulette-container">
       <PlayerWallet playerMoney={playerMoney} onHomeClick={onHomeClick} gameStarted={gameStarted}/>
       <div className="game-container">
-        <div className={`wheel ${isSpinning ? "spinning" : ""}`}>
+        <div className="wheel">
           <div className="numbers">
             {numbers.map((number) => (
-              <div key={number} className={`number ${number === result ? "winner" : ""}`}>
+              <div key={number} className={`number 
+                                           ${red.includes(number) ? "red" : ""}
+                                           ${black.includes(number) ? "black" : ""}
+                                           ${purple.includes(number) ? "purple" : ""}` }>
                 {number}
               </div>
             ))}
